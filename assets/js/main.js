ab1c359eced6a0ec57901830be260f8eac9deb79
vinimatio <vinimov@gmail.com>
2024-06-05 11:56:55 -0300

    headerDataUpdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
-(async () => {
+function updateProfileInfo(profileData) {
+    const avatar = document.getElementById('profile.photo');
+    avatar.src = profileData.photo;
+    avatar.alt = profileData.name;
 
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name;
+
+    const job = document.getElementById('profile.job');
+    job.innerText = profileData.job;
+
+    const location = document.getElementById('profile.location');
+    location.innerText = profileData.location;
+
+    const zap = document.getElementById('profile.zap');
+    zap.innerText = profileData.phone;
+    zap.href = `tel:+55${profileData.phone}`;
+
+    const email = document.getElementById('profile.mail');
+    email.innerText = profileData.email;
+    email.href = `mailto:${profileData.email}`;
+
+    
+}
+
+(async () => {
     const profileData = await fetchProfileData();
-    console.log(profileData);
+    updateProfileInfo(profileData);
 })()
